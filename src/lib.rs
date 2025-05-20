4d8c040cf33a76cb3f3a88525e622ef7bbb08ab8
Indigo Curnick <indigocurnick@gmail.com>
2025-05-20 16:10:53 +0100

    first

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+use std::fmt::Display;
+
+use time::{
+    Date, OffsetDateTime, PrimitiveDateTime, Time, format_description::well_known::Rfc3339,
+};
+
+pub enum Temporal {
+    Date(Date),
+    Time(Time),
+    DateTime(PrimitiveDateTime),
+    OffsetDateTime(OffsetDateTime),
+}
+
+impl TryFrom<&str> for Temporal {
+    type Error = HorologiumError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match Date::parse(value, &Rfc3339) {
+            Ok(x) => return Ok(Self::Date(x)),
+            Err(_) => {}
+        }
+
+        match Time::parse(value, &Rfc3339) {
+            Ok(x) => return Ok(Self::Time(x)),
+            Err(_) => {}
+        }
+
+        match PrimitiveDateTime::parse(value, &Rfc3339) {
+            Ok(x) => return Ok(Self::DateTime(x)),
+            Err(_) => {}
+        }
+
+        match OffsetDateTime::parse(value, &Rfc3339) {
+            Ok(x) => return Ok(Self::OffsetDateTime(x)),
+            Err(_) => {}
+        }
+
+        return Err(HorologiumError::UnableToDetermineType);
+    }
+}
+
+#[derive(Debug)]
+pub enum HorologiumError {
+    UnableToDetermineType,
+}
+
+impl Display for HorologiumError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::UnableToDetermineType => write!(
+                f,
+                "Unable to determine date-time type: are you sure this is a valid date/time string?"
+            ),
+        }
+    }
+}
